80a3808108a4a8409a5685459813ddfb39716af9
vaishakhp <vaishakhpat2003@gmail.com>
2023-10-20 15:48:36 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,57 @@
+
+
+window.addEventListener('load', () => {
+
+    document.getElementById('preloader').style.display = "none"
+    
+let toggleTheme = document.getElementById("toggleTheme")
+let themeIcons = document.querySelector('#themeIcons')
+var moonIcon = document.getElementById('moonIcon')
+var sunIcon = document.getElementById('sunIcon')
+
+toggleTheme.onclick =  function () {
+    document.body.classList.toggle("dark-theme")
+    if (document.body.classList.contains("dark-theme")) {
+        moonIcon.style.display = "none"
+        sunIcon.style.display = "block"
+
+    } else {
+        sunIcon.style.display = "none"
+        moonIcon.style.display = "block"
+      }
+    }
+    
+})
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml11 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml11 .line',
+    scaleY: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700
+  })
+  .add({
+    targets: '.ml11 .line',
+    translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+    easing: "easeOutExpo",
+    duration: 700,
+    delay: 100
+  }).add({
+    targets: '.ml11 .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=775',
+    delay: (el, i) => 34 * (i+1)
+  }).add({
+    targets: '.ml11',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
